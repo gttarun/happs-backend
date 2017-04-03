a348c6f1bfb336957b79a991db5a3027002a3c3c
Karim Sayani <krsayani11@gmail.com>
2017-04-02 22:38:31 -0400

    made pictures blank

--- events/models.py
+++ events/models.py
@@ -12,7 +12,7 @@ class Event(models.Model):
 	description = models.TextField()
 	longitude = models.CharField(max_length=255)
 	latitude = models.CharField(max_length=255)
-	picture = models.URLField()
+	picture = models.URLField(blank=True)
 
 	def __str__(self):
 		return self.event_name
